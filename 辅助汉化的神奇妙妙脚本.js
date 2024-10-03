544d24d15d854d274667b0faf463ba414b8d8a2c
Dave_12138 <207955131@qq.com>
2024-10-03 22:13:08 +0800

    不开游戏不好判断翻译啊

--- 辅助汉化的神奇妙妙脚本.js
+++ 辅助汉化的神奇妙妙脚本.js
@@ -8,15 +8,17 @@ function write(filename, content, rootDir) {
     const path = rootDir ? "./" : "./assets/butcher/lang/";
     fs.writeFileSync(path + filename, content, { encoding: "utf8", flag: "w+" });
 }
-const en0 = read('en_us.json');
+const en0 = read('old_en_us.json');
 const zh = read('zh_cn.json');
 const en = read('en_us.json', true);
 
-if (process.env.STEP !== "merge") {
+if (process.env.STEP?.trim() !== "merge") {
 
     console.log(Object.keys(en).filter(k => en[k] !== en0[k]).map(k => [k, en0[k], en[k], zh[k]].join('\t')).join('\n'))
     write("diff.json", JSON.stringify(Object.keys(en).filter(k => en[k] !== en0[k]).reduce((p, v) => Object.assign(p, { [v]: en[v] }), {})), true);
 } else {
     // 归并
-    // ((zh, diff) => { write("zh_cn.json",JSON.stringify(Object.keys(zh).concat(Object.keys(diff)).reduce((pv, v) => { pv[v] = diff[v] || zh[v] || ""; return pv }, {}))) })(zh, JSON.parse(l()))
+    const diff = read("diff.json", true);
+    write("zh_cn.json", JSON.stringify(Object.keys(zh).concat(Object.keys(diff)).reduce((pv, v) => Object.assign(pv, { [v]: diff[v] || zh[v] || "" }), {})))
+    write("old_en_us.json", JSON.stringify(en));
 }
