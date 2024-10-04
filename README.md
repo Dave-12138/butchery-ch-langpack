2f235a26a6540b25c6737c86c815c86caa38406d
Dave_12138 <207955131@qq.com>
2024-10-04 13:35:04 +0800

    v2.5.9
    试试github action

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # butchery汉化包
 
 ## 使用方法
-1. 点右上方那个绿色的 Code 按钮，然后点 Download Zip 
+1. 下载资源包
+    - 点[这个链接](./release)，下载`Butchery汉化.zip`
+    - 或者点右上方那个绿色的 Code 按钮，然后点 Download Zip 
 2. 把下载的压缩包放进 resourcepacks 文件夹里，右键点击压缩包，“解压到当前文件夹”
 3. 在游戏中启用
     - 依次进入“选项”、“资源包”
--- 辅助汉化的神奇妙妙脚本.js
+++ 辅助汉化的神奇妙妙脚本.js
@@ -10,15 +10,19 @@ function write(filename, content, rootDir) {
 }
 const en0 = read('old_en_us.json');
 const zh = read('zh_cn.json');
-const en = read('en_us.json', true);
+try {
+    const en = read('en_us.json', true);
 
-if (process.env.STEP?.trim() !== "merge") {
-    const changedKeys = Object.keys(en).filter(k => en[k] !== en0[k]);
-    write("diff2.json", changedKeys.map(k => [k, en0[k], en[k], zh[k]].map(r => r ?? "<empty>")), true);
-    write("diff.json", changedKeys.reduce((p, v) => Object.assign(p, { [v]: zh[v] ? zh[v] + `(${en[v]})` : en[v] }), {}), true);
-} else {
-    // 归并
-    const diff = read("diff.json", true);
-    write("zh_cn.json", Object.keys(zh).concat(Object.keys(diff)).reduce((pv, v) => Object.assign(pv, { [v]: diff[v] || zh[v] || "" }), {}));
-    write("old_en_us.json", en);
+    if (process.env.STEP?.trim() !== "merge") {
+        const changedKeys = Object.keys(en).filter(k => en[k] !== en0[k]);
+        write("diff2.json", changedKeys.map(k => [k, en0[k], en[k], zh[k]].map(r => r ?? "<empty>")), true);
+        write("diff.json", changedKeys.reduce((p, v) => Object.assign(p, { [v]: zh[v] ? zh[v] + `(${en[v]})` : en[v] }), {}), true);
+    } else {
+        // 归并
+        const diff = read("diff.json", true);
+        write("zh_cn.json", Object.keys(zh).concat(Object.keys(diff)).reduce((pv, v) => Object.assign(pv, { [v]: diff[v] || zh[v] || "" }), {}));
+        write("old_en_us.json", en);
+    }
+} catch (e) {
+    console.log(e)
 }
