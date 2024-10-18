772b30ad2dde7fc65faa4576811401e4c870875b
Dave_12138 <207955131@qq.com>
2024-10-18 20:08:21 +0800

    手册

--- 辅助汉化的神奇妙妙脚本.js
+++ 辅助汉化的神奇妙妙脚本.js
@@ -12,17 +12,21 @@ const en0 = read('old_en_us.json', true);
 const zh = read('zh_cn.json');
 try {
     const en = read('en_us.json', true);
-
-    if (process.env.STEP?.trim() !== "merge") {
-        const changedKeys = Object.keys(en).filter(k => en[k] !== en0[k]);
-        write("diff2.json", changedKeys.map(k => [k, en0[k], en[k], zh[k]].map(r => r ?? "<empty>")), true);
-        write("diff.json", changedKeys.reduce((p, v) => Object.assign(p, { [v]: zh[v] ? zh[v] + `(${en[v]})` : en[v] }), {}), true);
-    } else {
-        // 归并
-        const diff = read("diff.json", true);
-        write("zh_cn.json", Object.keys(zh).concat(Object.keys(diff)).reduce((pv, v) => Object.assign(pv, { [v]: diff[v] || zh[v] || "" }), {}));
-        write("old_en_us.json", en, true);
+    const uparam = {
+        diff() {
+            const changedKeys = Object.keys(en).filter(k => en[k] !== en0[k]);
+            write("diff.json", changedKeys.reduce((p, v) => Object.assign(p, { [v]: zh[v] ? zh[v] + `(${en[v]})` : en[v] }), {}), true);
+            write("diff2.json", changedKeys.map(k => [k, en0[k], en[k], zh[k]].map(r => r ?? "<empty>")), true);
+            write("diff3.json", Object.keys(zh).map(k => [k, en0[k], en[k], zh[k]].map(r => r ?? "<empty>")), true);
+        },
+        merge() {
+            // 归并
+            const diff = read("diff.json", true);
+            write("zh_cn.json", Object.keys(zh).concat(Object.keys(diff)).reduce((pv, v) => Object.assign(pv, { [v]: diff[v] || zh[v] || "" }), {}));
+            write("old_en_us.json", en, true);
+        },
     }
+    uparam[process.env.STEP?.trim()]?.();
 } catch (e) {
     console.log(e)
 }
